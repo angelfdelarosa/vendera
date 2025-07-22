b35b4002352cfd693c6a23fa3486f03bbadd9edd
Angel Felix <grizzlygames53@gmail.com>
2025-07-22 12:24:29 +0000

    Usa un WebView Wrapper con Capacitor para la versión de Android

---
+++ capacitor.config.ts
@@ -0,0 +1,11 @@
+
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.vendra.app',
+  appName: 'VENDRA',
+  webDir: 'out',
+  bundledWebRuntime: false,
+};
+
+export default config;
