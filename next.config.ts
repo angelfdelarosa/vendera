56626b33f369fb4abdcedac199f34563345e4e1b
Angel Felix <grizzlygames53@gmail.com>
2025-07-23 03:15:00 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
@@ -26,6 +27,10 @@ const nextConfig: NextConfig = {
       }
     ],
   },
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: 'https://tgmwexyipcwoppgikzrr.supabase.co',
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRnbXdleHlpcGN3b3BwZ2lrenJyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMyMzE2MzEsImV4cCI6MjA2ODgwNzYzMX0.DRQyAsyi1BtSLVJDHnr29Xo1bKVufPAjEbO-vAKq4F4',
+  }
 };
 
 export default nextConfig;
