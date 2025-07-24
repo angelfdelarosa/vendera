7a3e8d284e47415ea9d91839259dc8131e46550d
Angel Felix <grizzlygames53@gmail.com>
2025-07-24 18:42:52 +0000

    Changes before Firebase Studio auto-run

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"G7VN66Y3LM2XWbHyUnhdBvhpwJ4ymo1/CsF32o3lt4c=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
