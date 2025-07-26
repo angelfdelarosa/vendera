65cd9c0a14e9539295b1ee0d536173d2df6c827d
Angel Felix <grizzlygames53@gmail.com>
2025-07-26 23:11:22 +0000

    ReferenceError: useCallback is not defined     at ProfilePageClient (htt

--- .next/static/chunks/src_app_(main)_profile_[id]_page_tsx_fa8ef21c._.js
+++ .next/static/chunks/src_app_(main)_profile_[id]_page_tsx_fa8ef21c._.js
@@ -1,8 +1,8 @@
 (globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
-  "static/chunks/src_e0f20e30._.js",
-  "static/chunks/node_modules_0ee2ebc1._.js"
+  "static/chunks/src_537c71f7._.js",
+  "static/chunks/node_modules_e48dbd76._.js"
 ],
     source: "dynamic"
 });
